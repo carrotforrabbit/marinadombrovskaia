5c5cbfd5515d6786de545b360b8ec226fa5d9176
carrotforrabbit <parasyuta.anna@gmail.com>
2023-02-12 17:28:42 +0000

    change lighthouse

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,19 @@
 let modal = document.querySelector(".modal");
 let btn = document.querySelector(".education-btn");
+let workBtn = document.querySelector(".work-btn");
 let close = document.querySelector(".modal__cross");
 
 btn.addEventListener ("click", (e) => {
     modal.style.display = "block";
 })
 
+workBtn.addEventListener ("click", (e) => {
+    modal.style.display = "block";
+})
+
 close.addEventListener ("click", (e) => {
     modal.style.display = "none";
 })
-
-
-
 
 //up button
 let upBtn = document.querySelector('.up-button');
