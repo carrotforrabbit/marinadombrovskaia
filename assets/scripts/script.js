73b547b46c967dc478bd17b555fc85e1f0b50d42
carrotforrabbit <parasyuta.anna@gmail.com>
2023-02-17 15:11:11 +0000

    add new script for modals

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,27 +1,31 @@
-let modal = document.querySelector(".modal");
-let btn = document.querySelector(".education-btn");
-let workBtn = document.querySelector(".work-btn");
-let close = document.querySelector(".modal__cross");
+// let modal = document.querySelector(".modal");
+let btn = document.querySelectorAll(".open-modal");
+let closeBtn = document.querySelectorAll(".modal__cross");
 
-btn.addEventListener ("click", (e) => {
-    modal.style.display = "block";
-})
+// btn.addEventListener ("click", (e) => {
+//     modal.style.display = "block";
+// })
 
-workBtn.addEventListener ("click", (e) => {
-    modal.style.display = "block";
-})
+// workBtn.addEventListener ("click", (e) => {
+//     modal.style.display = "block";
+// })
 
-close.addEventListener ("click", (e) => {
-    modal.style.display = "none";
-})
+// close.addEventListener ("click", (e) => {
+//     modal.style.display = "none";
+// })
 
 //up button
-let upBtn = document.querySelector('.up-button');
+let upBtn = document.querySelector(".up-button");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
     upBtn.style.display = "block";
   } else {
     upBtn.style.display = "none";
@@ -32,3 +36,17 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+//modals
+Array.from(closeBtn, (closeButton) => {
+  closeButton.addEventListener(
+    "click",
+    (e) => (e.target.parentNode.parentNode.style.display = "none")
+  );
+});
+Array.from(btn, (openButton) => {
+  openButton.addEventListener("click", (e) => {
+    let modalId = e.target.getAttribute("data-id");
+    document.getElementById(modalId).style.display = "flex";
+  });
+});
