1e05d3e78602f130b566ba47e520151dea5c050d
carrotforrabbit <parasyuta.anna@gmail.com>
2023-03-10 13:19:15 +0000

    add FAQ, change help layout, minor bug fixes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -71,3 +71,23 @@ closeWork.addEventListener("click", (e) => {
   modalExperience.style.display = "none";
   document.body.classList.remove("stop-scrolling");
 });
+
+//details closure
+let details = document.querySelectorAll("details");
+for (i = 0; i < details.length; i++) {
+  details[i].addEventListener("toggle", accordion);
+}
+function accordion(event) {
+  if (!event.target.open) return;
+  var details = event.target.parentNode.children;
+  for (i = 0; i < details.length; i++) {
+    if (
+      details[i].tagName != "DETAILS" ||
+      !details[i].hasAttribute("open") ||
+      event.target == details[i]
+    ) {
+      continue;
+    }
+    details[i].removeAttribute("open");
+  }
+}
